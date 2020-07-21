6047027a222a88f1067305c7f8c6d7499768d754
Sean Mahoney <hello@seanmahoney.design>
2020-07-21 23:26:36 +0000

    Automatically backed up by Learn

--- spec/01_input_to_index_spec.rb
+++ spec/01_input_to_index_spec.rb
@@ -1,26 +1,72 @@
 require_relative "../lib/move.rb"
 
-describe '#input_to_index' do
-
-  it 'converts a user_input to an integer' do
-    user_input = "1"
-    converted_input = input_to_index(user_input)
-    
-    expect(converted_input).to be_a(Integer)
+describe './lib/move.rb' do
+  it 'defines a move method' do
+    board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    expect(defined?(move)).to be_truthy
   end
 
-  it 'subtracts 1 from the user_input' do
-    user_input = "6"
-    converted_input = input_to_index(user_input)
+  context '#move' do
+    it 'accepts 3 arguments: the board, the position a player wants to fill and their char, X or O' do
+      expect{move}.to raise_error(ArgumentError)
+    end
 
-    expect(converted_input).to be(5)
+    it 'provides a default value for the 3rd argument' do
+      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+      expect {move(board, 2)}.to_not raise_error
+    end
+
+    it 'allows "X" player in the top left position' do
+      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+      move(board, 1, "X")
+
+      expect(board).to eq(["X", " ", " ", " ", " ", " ", " ", " ", " "])      
+    end
+
+    it 'allows "O" player in the middle' do
+      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+      move(board, 5, "O")
+
+      expect(board).to eq([" ", " ", " ", " ", "O", " ", " ", " ", " "])      
+    end
+
+    it 'allows "X" player in the bottom right' do
+      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+      move(board, 9)
+
+      expect(board).to eq([" ", " ", " ", " ", " ", " ", " ", " ", "X"])
+    end
+
+    it 'allows "X" player in the bottom right and "O" in the top left ' do
+      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+      move(board, 1, "O")
+      move(board, 9, "X")
+
+      expect(board).to eq(["O", " ", " ", " ", " ", " ", " ", " ", "X"])
+    end
+
+    it 'allows "X" to win diagonally' do
+      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+      move(board, 1)
+      move(board, 5)
+      move(board, 9)
+
+      expect(board).to eq(["X", " ", " ", " ", "X", " ", " ", " ", "X"])
+    end
+
+    it 'allows a cats game' do
+      board = Array.new(9, " ")
+      move(board, 1, "X")
+      move(board, 2, "O")
+      move(board, 3, "X")      
+      move(board, 4, "O")
+      move(board, 5, "X")
+      move(board, 6, "O")      
+      move(board, 7, "X")
+      move(board, 8, "X")
+      move(board, 9, "O")      
+
+      expect(board).to eq(["X", "O", "X", "O", "X", "O", "X", "X", "O"])
+    end
   end
-
-  it 'returns -1 for strings without integers' do
-    user_input = "invalid"
-    converted_input = input_to_index(user_input)
-    
-    expect(converted_input).to be(-1)
-  end
-
 end
